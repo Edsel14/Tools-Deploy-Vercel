b9d3a48799b9581fe38526db8877d4e320b54037
Edsel14 <edsel6836@gmail.com>
2026-09-04 14:50:48 +0700

    Add PrismaNeon adapter to PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL || ""
+});
+
+export const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
